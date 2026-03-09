cb11e3fa58f681f00e95efef178edef94bfc885b
Kalpana <kalpana.jas.digicrafttek@gmail.com>
2026-03-09 18:35:51 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1 className='text-2xl text-center mt-4 text-red-600'>Hello Kalpzz!</h1>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // must match your files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
